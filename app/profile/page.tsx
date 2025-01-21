f88bab179b51e819c305f7637b9c99ee63b10123
yu-rimasui <usagi.y12@gmail.com>
2025-01-22 01:42:44 +0900

    プロフィール修正

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -50,21 +50,25 @@ export default function Profile() {
               趣味は最新のAIツールを色々試したり、実装して日常の生活やタスクを効率化させること。
               <br />
               <br />
-              プログラミングスクールで約1年間Web開発を学んだのち、通信系企業の開発部でインターンとして1年間実務経験を積みました。
+              プログラミングスクールで約1年間Web開発を学び、その後、通信系企業の開発部で1年間インターンとして実務経験を積みました。
               <br />
-              現在はクライアントワークを通じて実績を積み上げています
+              現在はクライアントワークを通じて着実に実績を積み重ねています。
               <br />
               <br />
-              新しい技術やトレンドのキャッチアップを欠かさず、日々成長し続けることがモットー。自分の可能性は無限大✨
+              新しい技術やトレンドをキャッチアップしながら、日々成長することが私のモットー。可能性は行動次第で無限大✨
               <br />
             </p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5, delay: 0.4 }}
-            className="text-white"
-          >
+            <div className="my-8">
+              <Image
+                src="/images/about.jpg"
+                alt="プロフィール画像"
+                width={250}
+                height={250}
+                className="rounded-full mx-auto shadow-xl object-cover border-2 border-gray-300 p-1 bg-white ring-2 ring-indigo-300"
+              />
+            </div>
+            <p>平成14年生まれ、東京育ち。SFオタク。</p>
+            <br />
             <h2 className="text-2xl font-semibold mb-4">スキル</h2>
             <div className="flex flex-wrap gap-2 mb-4">
               {skills.map((skill) => (
@@ -76,25 +80,44 @@ export default function Profile() {
                 </span>
               ))}
             </div>
-            <br />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            className="text-white"
+          >
             <h2 className="text-2xl font-semibold mb-4">できること</h2>
             <p className="mb-4">
               <strong>フロントエンド開発</strong>
+              <br /> <br />
+              ・HTML・CSS：レスポンシブデザイン対応のランディングページ（LP）や、ページ遷移可能なホームページ（HP）の作成
               <br />
-              ・レスポンシブデザイン対応のWebページやランディングページ（LP）の作成
+              ・JavaScript：インタラクティブな機能を備えたWebサイトの構築
               <br />
-              ・ReactやNext.jsを用いたた、高速で直感的なWebサイトやホームページ（HP）の構築
+              ・React・Next.js：高速かつ直感的なWebアプリケーションやサイトの開発
               <br />
               <br />
-              <strong>業務自動化サポート</strong>
+              <strong>バックエンド開発</strong>
+              <br /> <br />
+              ・Node.js・MySQL：データベース連携が可能なWebアプリケーションの構築
               <br />
-              ・GASやDifyなどを活用した業務自動化の導入支援
+              ・AWS（学習中）：クラウド環境を活用したWebサービスの運用
               <br />
-              <strong>データ分析（学習中）</strong>
               <br />
-              ・Pythonのライブラリを使用した分析
+              <strong>業務自動化・効率化サポート</strong>
+              <br /> <br />
+              ・Google関連ツール（Google Apps Script、BigQuery、Looker
+              Studio）：スプレッドシートやGoogleフォームを活用した自動化ツールの作成、大規模データのクエリ処理や可視化、データレポートの作成
               <br />
-              ・Kaggleのコンペに挑戦しています
+              ・Dify：チャットボットやAIを活用した業務効率化システムの導入支援
+              <br />
+              <br />
+              <strong>データ分析</strong>
+              <br /> <br />
+              ・Python：PandasやMatplotlibなどのライブラリを使用したデータ分析やレポート作成
+              <br />
+              ・Kaggle：データ分析や機械学習のスキル向上のためコンペに挑戦中
               <br />
             </p>
           </motion.div>
